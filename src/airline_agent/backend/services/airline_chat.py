a768e642678590a35c71034acd250067f32e33e3
Angela <angela@cleanlab.ai>
2025-10-21 14:20:38 -0700

    add metadata

--- src/airline_agent/backend/services/airline_chat.py
+++ src/airline_agent/backend/services/airline_chat.py
@@ -4,17 +4,14 @@ import pathlib
 import uuid
 from collections.abc import AsyncGenerator
 
+from codex.types import ProjectValidateResponse
 from dotenv import load_dotenv
 from pydantic_ai import (
     CallToolsNode,
     ModelMessage,
-    ModelRequest,
     ModelRequestNode,
-    ModelResponse,
-    TextPart,
     ToolCallPart,
     ToolReturnPart,
-    UserPromptPart,
 )
 from pydantic_ai.run import End
 
@@ -25,9 +22,9 @@ from src.airline_agent.cleanlab_utils.validate_utils import (
 )
 from src.airline_agent.tools.knowledge_base import KnowledgeBase
 from src.airline_agent.backend.schemas.message import (
-    AgentMessage,
     AssistantMessage,
-    MessageRole,
+    EvalResult,
+    MessageMetadata,
     ToolCall,
     ToolCallMessage,
     UserMessage,
@@ -125,7 +122,7 @@ async def airline_chat_streaming(
 
             elif isinstance(node, End):
                 if run.result is not None:
-                    updated_message_history, final_response = (
+                    updated_message_history, final_response, validation_result = (
                         run_cleanlab_validation_logging_tools(
                             project=project,
                             query=user_prompt,
@@ -141,10 +138,21 @@ async def airline_chat_streaming(
                         data=AssistantMessage(
                             thread_id=thread_id,
                             content=final_response,
+                            metadata=MessageMetadata(
+                                original_llm_response=run.result.output,
+                                is_expert_answer=validation_result.expert_answer
+                                is not None,
+                                guardrailed=validation_result.should_guardrail,
+                                escalated_to_sme=validation_result.escalated_to_sme,
+                                scores=_format_eval_results(validation_result),
+                            ),
                         ),
                     )
                     thread_to_messages[thread_id] = updated_message_history
                 else:
+                    logger.warning(
+                        "Unable to validate response with cleanlab. Missing `run.result`."
+                    )
                     yield RunEventThreadMessage(
                         id=run_id,
                         object=RunEventObject.THREAD_MESSAGE,
@@ -155,36 +163,6 @@ async def airline_chat_streaming(
                     )
                     thread_to_messages[thread_id] = run.ctx.state.message_history.copy()
 
-    # eval_scores = {
-    #     eval_key: EvalResult.model_validate(eval_result.model_dump())
-    #     for eval_key, eval_result in response["validation_result"].eval_scores.items()
-    # }
-    # deterministic_guardrails_results = (
-    #     {
-    #         guardrail_key: EvalResult(
-    #             score=0.0 if guardrail_result.should_guardrail else None,
-    #             triggered=guardrail_result.should_guardrail,
-    #             triggered_guardrail=guardrail_result.should_guardrail,
-    #         )
-    #         for guardrail_key, guardrail_result in response[
-    #             "validation_result"
-    #         ].deterministic_guardrails_results.items()
-    #     }
-    #     if response["validation_result"].deterministic_guardrails_results is not None
-    #     else {}
-    # )
-    # return Message(
-    #     thread_id=thread_id,
-    #     role=MessageRole.ASSISTANT,
-    #     content=response["response"],
-    #     metadata=MessageMetadata(
-    #         original_llm_response=response["original_response"],
-    #         is_expert_answer=response["validation_result"].expert_answer is not None,
-    #         guardrailed=response["validation_result"].should_guardrail,
-    #         escalated_to_sme=response["validation_result"].escalated_to_sme,
-    #         scores={**eval_scores, **deterministic_guardrails_results},
-    #     ),
-    # )
     yield RunEventThreadRunCompleted(
         id=run_id,
         object=RunEventObject.THREAD_RUN_COMPLETED,
@@ -196,103 +174,23 @@ async def airline_chat_streaming(
     )
 
 
-def _convert_message_history_to_pydantic_ai_messages(
-    messages: list[AgentMessage],
-) -> list[ModelMessage]:
-    message_history: list[ModelMessage] = []
-    for message in messages:
-        if message.role == MessageRole.USER:
-            assert isinstance(message.content, str)
-            message_history.append(
-                ModelRequest(parts=[UserPromptPart(content=message.content)])
+def _format_eval_results(
+    validation_result: ProjectValidateResponse,
+) -> dict[str, EvalResult]:
+    eval_scores = {
+        eval_key: EvalResult.model_validate(eval_result.model_dump())
+        for eval_key, eval_result in validation_result.eval_scores.items()
+    }
+    deterministic_guardrails_results = (
+        {
+            guardrail_key: EvalResult(
+                score=0.0 if guardrail_result.should_guardrail else None,
+                triggered=guardrail_result.should_guardrail,
+                triggered_guardrail=guardrail_result.should_guardrail,
             )
-        elif message.role == MessageRole.ASSISTANT:
-            assert isinstance(message.content, str)
-            message_history.append(
-                ModelResponse(parts=[TextPart(content=message.content)])
-            )
-        elif message.role == MessageRole.TOOL:
-            assert isinstance(message.content, ToolCall)
-            message_history.append(
-                ModelResponse(
-                    parts=[
-                        ToolCallPart(
-                            tool_call_id=message.content.tool_call_id,
-                            tool_name=message.content.tool_name,
-                            args=json.loads(message.content.arguments)
-                            if message.content.arguments
-                            else None,
-                        )
-                    ]
-                )
-            )
-            message_history.append(
-                ModelRequest(
-                    parts=[
-                        ToolReturnPart(
-                            tool_name=message.content.tool_name,
-                            tool_call_id=message.content.tool_call_id,
-                            content=message.content.result,
-                        )
-                    ]
-                )
-            )
-    return message_history
-
-
-def _convert_pydantic_ai_messages_to_message_history(
-    messages: list[ModelMessage], thread_id: str
-) -> list[AgentMessage]:
-    formatted_message_history: list[AgentMessage] = []
-    current_tool_calls: dict[str, ToolCall] = {}
-    for message in messages:
-        if isinstance(message, ModelRequest):
-            if len(message.parts) == 0:
-                logger.warning("Empty ModelRequest message received")
-                continue
-
-            if isinstance(message.parts[0], UserPromptPart):
-                content = message.parts[0].content
-                for part in message.parts:
-                    if isinstance(part, UserPromptPart):
-                        content += part.content
-
-                formatted_message_history.append(
-                    UserMessage(thread_id=thread_id, content=content).model_dump()
-                )
-            elif isinstance(message.parts[0], ToolReturnPart):
-                for part in message.parts:
-                    formatted_message_history.append(
-                        ToolCallMessage(
-                            thread_id=thread_id,
-                            content=ToolCall(
-                                tool_call_id=part.tool_call_id,
-                                tool_name=part.tool_name,
-                                arguments=current_tool_calls[
-                                    part.tool_call_id
-                                ].arguments,
-                                result=part.model_response_str(),
-                            ),
-                        ).model_dump()
-                    )
-                    del current_tool_calls[part.tool_call_id]
-
-        elif isinstance(message, ModelResponse):
-            if message.finish_reason == "tool_call":
-                for part in message.parts:
-                    if isinstance(part, ToolCallPart):
-                        current_tool_calls[part.tool_call_id] = ToolCall(
-                            tool_call_id=part.tool_call_id,
-                            tool_name=part.tool_name,
-                            arguments=json.dumps(part.args),
-                        )
-            else:
-                content = ""
-                for part in message.parts:
-                    if isinstance(part, TextPart):
-                        content += part.content
-                formatted_message_history.append(
-                    AssistantMessage(thread_id=thread_id, content=content).model_dump()
-                )
-
-    return formatted_message_history
+            for guardrail_key, guardrail_result in validation_result.deterministic_guardrails_results.items()
+        }
+        if validation_result.deterministic_guardrails_results is not None
+        else {}
+    )
+    return {**eval_scores, **deterministic_guardrails_results}
