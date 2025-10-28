d53eb20a3110d83f8a51d5bd315aeba26418066e
Angela Liu <angela@cleanlab.ai>
2025-10-28 11:04:08 -0700

    support running create-demo-project in non-admin account (#48)

--- src/airline_agent/cleanlab_utils/create_new_demo_project.py
+++ src/airline_agent/cleanlab_utils/create_new_demo_project.py
@@ -4,31 +4,14 @@ import argparse
 import logging
 import os
 
-import httpx
 from cleanlab_codex import Client
-from codex.types.project_return_schema import ProjectReturnSchema
 from dotenv import load_dotenv
-from httpx import URL
 
 from airline_agent.constants import OFFICIAL_DEMO_PROJECT_ID
 
 DEFAULT_DEMO_PROJECT_NAME = "(Demo) Frontier Airlines Support Chatbot"
 DEFAULT_DEMO_PROJECT_DESCRIPTION = "Do not delete please!"
-COPY_PROJECT_ROUTE = "api/admin/projects/copy_project_settings"
 logger = logging.getLogger(__name__)
-
-
-def copy_project_configuration(new_project_id: str, api_key: str, base_url: URL) -> ProjectReturnSchema:
-    with httpx.Client(headers={"X-API-Key": api_key}) as client:
-        response = client.patch(
-            base_url.join(COPY_PROJECT_ROUTE),
-            params={
-                "project_id": OFFICIAL_DEMO_PROJECT_ID,
-                "new_project_id": new_project_id,
-            },
-        )
-        response.raise_for_status()
-        return ProjectReturnSchema.model_validate(response.json())
 
 
 def main() -> None:
@@ -59,20 +42,15 @@ def main() -> None:
 
     codex_client = Client(api_key=codex_api_key)
 
-    new_project = codex_client.create_project(
+    new_project = codex_client.create_project_from_template(
+        template_project_id=OFFICIAL_DEMO_PROJECT_ID,
         name=args.name,
         description=args.description,
     )
 
-    new_project_with_updated_config = copy_project_configuration(
-        new_project.id,
-        codex_api_key,
-        base_url=codex_client._client.base_url,  # noqa: SLF001
-    )
-
     print("Demo project created!")  # noqa: T201
     print(  # noqa: T201
-        f"Set CLEANLAB_PROJECT_ID={new_project_with_updated_config.id} in your .env file."
+        f"Set CLEANLAB_PROJECT_ID={new_project.id} in your .env file."
     )
 
 
